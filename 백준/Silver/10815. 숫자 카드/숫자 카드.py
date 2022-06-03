af67284c85ec85486250b2aed2e28a2aa3ea8521
태라 <82090621+Terra2007@users.noreply.github.com>
2022-06-03 23:27:41 +0900

    [Silver IV] Title: 숫자 카드, Time: 3488 ms, Memory: 111212 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -0,0 +1,18 @@
+n = int(input())
+arr = sorted(list(map(int, input().split())))
+m = int(input())
+find = list(map(int, input().split()))
+
+def binary_search(arr, value, left, right):
+    middle = (left + right)//2
+    if(left > right):
+        return 0;
+    elif(value == arr[middle]):
+        return 1;
+    elif(value < arr[middle]):
+        return binary_search(arr, value, left, middle-1)
+    else:
+        return binary_search(arr, value, middle+1, right)
+
+for i in find:
+    print(binary_search(arr, i, 0, len(arr)-1), end=" ")
