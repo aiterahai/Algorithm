2cfac5aa0fa6fdf4d0a122d66ced885f949b9a17
태라 <82090621+Terra2007@users.noreply.github.com>
2022-06-04 20:49:32 +0900

    [Silver III] Title: N과 M (7), Time: 1756 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+visitors = sorted(map(int, input().split()))
+visited = []
+
+def dfs(k):
+    if(len(visited) == m):
+        print(' '.join(map(str, visited)))
+    else:
+        for i in visitors:
+            visited.append(i)
+            dfs(k+1)
+            visited.pop()
+
+dfs(0)
