430836645a7e366a7791f020b23d6717fd316128
태라 <82090621+Terra2007@users.noreply.github.com>
2022-08-06 02:35:04 +0900

    [Gold V] Title: 용액, Time: 168 ms, Memory: 129724 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.py
@@ -0,0 +1,12 @@
+from sys import stdin
+
+N = int(stdin.readline())
+board = sorted(list(map(int, stdin.readline().split())))
+result = [1000000000, 1000000000]
+start, end = 0, N - 1
+while start < end:
+    temp = board[start] + board[end]
+    if abs(temp) < abs(sum(result)): result = [board[start], board[end]]
+    if temp < 0: start += 1
+    else: end -= 1
+print(*(sorted(result)))
