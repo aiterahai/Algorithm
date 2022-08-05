a8ab6a46c40b29f7a0eb3dede655e819b7e97a66
태라 <82090621+Terra2007@users.noreply.github.com>
2022-08-05 17:00:18 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 484 ms, Memory: 147728 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,22 @@
+from sys import stdin
+from collections import deque
+
+N, M, R = map(int, stdin.readline().split())
+graph = [[] for _ in range(N + 1)]
+visit = [0] * (N + 1)
+for _ in range(M):
+    A, B = map(int, stdin.readline().split())
+    graph[A].append(B)
+    graph[B].append(A)
+for i in range(1, N + 1): graph[i] = sorted(graph[i], reverse=True)
+Q = deque([R])
+count = 1
+visit[R] = count
+while Q:
+    node = Q.popleft()
+    for i in graph[node]:
+        if visit[i]: continue
+        count += 1
+        visit[i] = count
+        Q.append(i)
+print(*visit[1:], sep="\n")
